31bbaf0280bc79c10897bd1da64b7a86db419a96
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2026-01-21 16:26:57 +0200

    add html

--- components/IncomeForm/IncomeForm.tsx
+++ components/IncomeForm/IncomeForm.tsx
@@ -1,15 +1,73 @@
 import css from "./IncomeForm.module.css";
 
-const IncomeForm = () => {
+const IncomePage = () => {
   return (
-    <div className={css.incomeForm}>
-      <h1 className={css.textTitle}>All Income</h1>
-      <p className={css.textPage}>
-        Track and celebrate every bit of earnings effortlessly! Gain insights
-        into your total revenue in a snap.
-      </p>
+    <div className={css.incomePage}>
+      <div className={css.title}>
+        <h1 className={css.titleText}>All Income</h1>
+        <p className={css.titleParagraf}>
+          Track and celebrate every bit of earnings effortlessly! Gain insights
+          into your total revenue in a snap.
+        </p>
+      </div>
+      <div className={css.total}>
+        <div className={css.totalIncome}>
+          <svg></svg>
+          <ul>
+            <li>Total Income</li>
+            <li>$909.000</li>
+          </ul>
+        </div>
+        <div className={css.totalExpense}>
+          <svg></svg>
+          <ul>
+            <li>Total Expense</li>
+            <li>$259.000</li>
+          </ul>
+        </div>
+      </div>
+      <div className={css.incomeForm}>
+        <form>
+          <input>Search for anything..</input>
+          <input>dd/mm/yyyy</input>
+        </form>
+        <div>
+          <ul>
+            <li>
+              Category<p>Salary</p>
+            </li>
+
+            <li>
+              Comment<p>IT company</p>
+            </li>
+
+            <li>
+              Date
+              <p>Sn, 3.03.2023</p>
+            </li>
+
+            <li>
+              Time
+              <p>14:30</p>
+            </li>
+
+            <li>
+              Sum
+              <p>35 000 / UAH</p>
+            </li>
+
+            <li>
+              Actions
+              <div>
+                <button>Edit</button>
+                <button>Delete</button>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default IncomeForm;
+export default IncomePage;
