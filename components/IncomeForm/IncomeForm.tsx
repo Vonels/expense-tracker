08928be1ac8c97a233fe52973e253350db3d224e
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2026-01-21 14:51:04 +0200

    added html incomeform and add css

---
+++ components/IncomeForm/IncomeForm.tsx
@@ -0,0 +1,15 @@
+import css from "./IncomeForm.module.css";
+
+const IncomeForm = () => {
+  return (
+    <div className={cssincomeForm}>
+      <h1 className={css.textTitle}>All Income</h1>
+      <p className={css.textPage}>
+        Track and celebrate every bit of earnings effortlessly! Gain insights
+        into your total revenue in a snap.
+      </p>
+    </div>
+  );
+};
+
+export default IncomeForm;
