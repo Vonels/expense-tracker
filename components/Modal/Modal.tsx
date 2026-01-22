2575ddfcc192dd3d804f510a162d76827543798a
PolinaGr <polinasoukh@gmail.com>
2026-01-22 18:51:03 +0200

    add modal

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,96 +1,48 @@
+"use client";
 import { useEffect } from "react";
 import { createPortal } from "react-dom";
+import { useRouter } from "next/navigation";
 import css from "./Modal.module.css";
 
 interface ModalProps {
-  onClose: () => void;
+  children: React.ReactNode;
 }
 
-const Modal = ({ onClose }: ModalProps) => {
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
+export const Modal = ({ children }: ModalProps) => {
+  const router = useRouter();
+  const onClose = () => router.back();
 
-    const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.code === "Escape") onClose();
+  useEffect(() => {
+    const handleEscape = (e: KeyboardEvent) => {
+      if (e.code === "Escape") router.back();
     };
 
-    window.addEventListener("keydown", handleKeyDown);
+    document.body.style.overflow = "hidden";
+    window.addEventListener("keydown", handleEscape);
 
     return () => {
       document.body.style.overflow = "unset";
-      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keydown", handleEscape);
     };
-  }, [onClose]);
-
-  const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (e.target === e.currentTarget) onClose();
-  };
+  }, [router]);
 
   return createPortal(
     <div
       className={css.backdrop}
-      onClick={handleBackdropClick}
-      role="dialog"
-      aria-modal="true"
+      onClick={(e) => e.target === e.currentTarget && router.back()}
     >
       <div className={css.modal}>
-        <button className={css.closeBtnCategoriesModal}>
+        <button
+          className={css.closeBtnCategoriesModal}
+          onClick={() => router.back()}
+        >
           <svg width="12" height="12">
-            <use href="/path-to-your-icons.svg#icon-check"></use>
+            <use href="/symbol-defs.svg#icon-Close"></use>
           </svg>
         </button>
-
-        <h2 className={css.titleCategoriesModal}>Expenses</h2>
-        <p className={css.allCategoryTxt}>All Category</p>
-
-        <ul className={css.listCategoriesModal}>
-          <li className={`${css.itemCategory} ${css.chosenCategoryBg}`}>
-            <span>Cinema</span>
-            <div className={css.actionBtns}>
-              <button className={css.iconBtnCheck}>
-                <svg width="12" height="12">
-                  <use href="/path-to-your-icons.svg#icon-check"></use>
-                </svg>
-              </button>
-              <button className={css.iconBtnEdit}>
-                <svg width="12" height="12">
-                  <use href="/path-to-your-icons.svg#icon-check"></use>
-                </svg>
-              </button>
-              <button className={css.iconBtnDelete}>
-                <svg width="12" height="12">
-                  <use href="/path-to-your-icons.svg#icon-check"></use>
-                </svg>
-              </button>
-            </div>
-          </li>
-
-          <li className={css.itemCategory}>
-            <span>Products</span>
-          </li>
-
-          <li className={css.itemCategory}>
-            <span>Clothes</span>
-          </li>
-          <li className={css.itemCategory}>
-            <span>Education</span>
-          </li>
-        </ul>
-
-        <div className={css.footerModal}>
-          <p className={css.newCategoryTxt}>New Category</p>
-          <div className={css.inputWrapper}>
-            <input
-              className={css.inputCategoriesModal}
-              placeholder="Enter the text"
-            />
-            <button className={css.btnCategoriesModal}>Add</button>
-          </div>
-        </div>
+        {children}
       </div>
     </div>,
     document.body,
   );
 };
-
-export default Modal;
