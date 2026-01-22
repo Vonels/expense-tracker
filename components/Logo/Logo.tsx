79cd29bc3544d78bca5a6664a54ec1778555e9dd
Vonels <klizka38@gmail.com>
2026-01-22 17:51:44 +0200

    add base

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,15 +1,20 @@
-'use client';
+"use client";
 import Link from "next/link";
 import { useAuthStore } from "@/lib/store/authStore";
-import css from './Logo.module.css'
+import css from "./Logo.module.css";
 
 const Logo = () => {
-    const { isLoggedIn } = useAuthStore();
-    const navigateTo = isLoggedIn
-        ? '/transactions' // MainTransactionsPage
-        : '/';             // WelcomePage
-    return <Link href={navigateTo} aria-label="Home" className={css.link}>
-        <svg className={css.logoSvg} width={27} height={16}><use href="../../public/symbol-defs.svg#icon-Logo"/></svg>Expensetracker
+  const { isLoggedIn } = useAuthStore();
+  const navigateTo = isLoggedIn
+    ? "/transactions" // MainTransactionsPage
+    : "/"; // WelcomePage
+  return (
+    <Link href={navigateTo} aria-label="Home" className={css.link}>
+      <svg className={css.logoSvg} width={27} height={16}>
+        <use href="../../public/symbol-defs.svg#icon-Logo" />
+      </svg>
+      Expensetracker
     </Link>
+  );
 };
 export default Logo;
