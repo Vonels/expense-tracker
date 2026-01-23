2339c9ed90939f9d481609f4afe993eaaee10c72
bohdan_berest <berestbodi@gmail.com>
2026-01-23 10:46:25 +0200

    dashboard css

--- app/(private)/dashboard/page.tsx
+++ app/(private)/dashboard/page.tsx
@@ -13,30 +13,30 @@ export default function DashboardPage() {
 
   return (
     <main className={css.container}>
-      <div className={css.info}>
-        <h1 className={css.title}>Expense Log</h1>
-        <p className={css.subtitle}>
-          Capture and organize every penny spent with ease! A clear view of your
-          financial habits at your fingertips.
-        </p>
-      </div>
-
       <div className={css.mainLayout}>
         <section className={css.statsSection}>
+          <div className={css.info}>
+            <h1 className={css.title}>Expense Log</h1>
+            <p className={css.subtitle}>
+              Capture and organize every penny spent with ease! A clear view of
+              your financial habits at your fingertips.
+            </p>
+          </div>
+
           <div className={css.summaryCards}>
             <TotalIncome />
             <TotalExpense />
           </div>
           <ExpensesChart />
         </section>
-      </div>
 
-      <aside className={css.formSection}>
-        <TransactionForm
-          onOpenCategories={handleOpenCategories}
-          selectedCategoryName="Category"
-        />
-      </aside>
+        <aside className={css.formSection}>
+          <TransactionForm
+            onOpenCategories={handleOpenCategories}
+            selectedCategoryName="Category"
+          />
+        </aside>
+      </div>
     </main>
   );
 }
