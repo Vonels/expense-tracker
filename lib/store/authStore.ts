56f33833ff0fbe033ea4b58a730cc5d68dfdaa9d
Tenebris An <y2222672@gmail.com>
2026-01-23 19:29:15 +0200

    update loader

--- lib/store/authStore.ts
+++ lib/store/authStore.ts
@@ -1,3 +1,48 @@
+// import { create } from "zustand";
+// import { persist } from "zustand/middleware";
+
+// interface User {
+//   name: string;
+//   email: string;
+//   avatarUrl?: string;
+//   currency: string;
+// }
+
+// interface AuthState {
+//   user: User | null;
+//   token: string | null;
+//   isLoggedIn: boolean;
+//   isLoading: boolean;
+//   setLoading: (status: boolean) => void;
+//   setAuthData: (user: User, token: string) => void;
+//   updateUser: (data: Partial<User>) => void;
+//   logout: () => void;
+// }
+
+// export const useAuthStore = create<AuthState>()(
+//   persist(
+//     (set) => ({
+//       user: null,
+//       token: null,
+//       isLoggedIn: false,
+//       isLoading: false,
+
+//       setLoading: (status) => set({ isLoading: status }),
+
+//       setAuthData: (user, token) => set({ user, token, isLoggedIn: true }),
+
+//       updateUser: (data) =>
+//         set((state) => ({
+//           user: state.user ? { ...state.user, ...data } : null,
+//         })),
+
+//       logout: () => set({ user: null, token: null, isLoggedIn: false }),
+//     }),
+//     {
+//       name: "auth-storage",
+//     },
+//   ),
+// );
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
@@ -40,6 +85,11 @@ export const useAuthStore = create<AuthState>()(
     }),
     {
       name: "auth-storage",
-    },
-  ),
+      partialize: (state) => ({
+        user: state.user,
+        token: state.token,
+        isLoggedIn: state.isLoggedIn,
+      }),
+    }
+  )
 );
