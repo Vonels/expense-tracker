a18f50c1bb18a6af758611ece08805c2ff869cb4
Valeri <valeriakoval27072007@gmail.com>
2026-01-23 19:46:40 +0100

    dvd

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,60 @@
+"use client";
+
 import css from "./page.module.css";
 import { Icon } from "@/components/Icon/Icon";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <main>
-        <div>
-          <div>
-            <div>
-              <div>
-                <Icon />
-              </div>
-              <div>
-                <p>Your balance</p>
-                <p>$632.000</p>
-              </div>
-              <div>
-                <p>+1.29%</p>
-              </div>
-            </div>
-          </div>
-          <div>
-            <p></p>
-            <div>
-              <p>Manage Your Finances Masterfully!</p>
-              <p>
-                ExpenseTracker effortlessly empowers you to take control of your
-                finances! With intuitive features, it simplifies the process of
-                tracking and managing expenses, allowing for a stress-free
-                mastery over your financial world.
-              </p>
+    <main>
+      <div className={css.wrapper}>
+        <div className={css.backgroundphoto}>
+          <div className={css.rectangle}>
+            <div className={css.svg}>
+              <Icon id="icon-arrow-up-right2" className={css.icon} />
             </div>
             <div>
-              <button type="button"></button>
-              <button type="button"></button>
+              <p className={css.rectangle_text1}>Your balance</p>
+              <p className={css.rectangle_text2}>$632.000</p>
             </div>
-            <div>
-              <Image alt="avatars" src={""} />
-              <p></p>
+            <div className={css.rectangle_block2}>
+              <p className={css.rectangle_text3}>+1.29%</p>
             </div>
           </div>
         </div>
-      </main>
-    </>
+        <div>
+          <p className={css.form_text1}>Expense Log</p>
+          <div className={css.form_block1}>
+            <p className={css.form_text2}>
+              Manage Your <span className={css.span}>Finances</span>{" "}
+              Masterfully!
+            </p>
+            <p className={css.form_text3}>
+              ExpenseTracker effortlessly empowers you to take control of your
+              finances! With intuitive features, it simplifies the process of
+              tracking and managing expenses, allowing for a stress-free mastery
+              over your financial world.
+            </p>
+          </div>
+          <div className={css.form_block2}>
+            <button className={css.button_signup} type="button">
+              Sign Up
+            </button>
+            <button className={css.button_signin} type="button">
+              Sign In
+            </button>
+          </div>
+          <div className={css.form_block3}>
+            <Image className={css.image} alt="avatars" src={""} />
+            <div>
+              <p className={css.form_text4}> 1000 users + </p>
+              <p className={css.form_text5}>
+                Trusted by users for reliable expense tracking!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
