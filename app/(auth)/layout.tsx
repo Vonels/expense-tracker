dec5d59f4292509b77b94778eb07013207b8932b
Valeri <valeriakoval27072007@gmail.com>
2026-01-26 15:51:08 +0100

    layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,33 @@
+"use client";
+
+import css from "./page.module.css";
+import { Icon } from "@/components/Icon/Icon";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <main className={css.mainContainer}>
+      <div className={css.wrapper}>
+        <div className={css.backgroundphoto}>
+          <div className={css.rectangle}>
+            <div className={css.svg}>
+              <Icon id="icon-arrow-up-right2" className={css.icon} />
+            </div>
+            <div>
+              <p className={css.rectangle_text1}>Your balance</p>
+              <p className={css.rectangle_text2}>$632.000</p>
+            </div>
+            <div className={css.rectangle_block2}>
+              <p className={css.rectangle_text3}>+1.29%</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={css.contentSide}>{children}</div>
+      </div>
+    </main>
+  );
 }
