067c657b4c1061bdb1da50efa221039b2cb9e023
Tenebris An <y2222672@gmail.com>
2026-01-26 17:11:59 +0200

    merge main

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -6,3 +6,22 @@ export const api = axios.create({
 });
 
 console.log("API URL:", process.env.NEXT_PUBLIC_API_URL_BACKEND);
+// import axios from "axios";
+// import { useAuthStore } from "../store/authStore";
+
+// const baseURL = process.env.NEXT_PUBLIC_API_URL_BACKEND;
+
+// export const api = axios.create({
+//   baseURL,
+//   withCredentials: true,
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = useAuthStore.getState().token;
+//   console.log("DEBUG: Токен в інтерцепторі:", token);
+
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
