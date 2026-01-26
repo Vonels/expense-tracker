a4153a7075a357e282e09c2b1b94efea8cc35d7b
Iryna Polyvana <irynapolyvana@gmail.com>
2026-01-26 21:10:41 +0100

    fix HeaderGuest and add styles

--- components/HeaderGuest/HeaderGuest.tsx
+++ components/HeaderGuest/HeaderGuest.tsx
@@ -1,7 +1,12 @@
 import Logo from "../Logo/Logo"
+import css from './HeaderGuest.module.css'
 
 const HeaderGuest = () => {
-    <Logo />
+    return (<header className='container'>
+        <div className={css.header}>
+            <Logo/>
+        </div>
+    </header>)
 }
 
 export default HeaderGuest;
