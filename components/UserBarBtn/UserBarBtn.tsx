a26600a41b74f2dcfa15136b3785b2e8d8de3b1f
Iryna Polyvana <irynapolyvana@gmail.com>
2026-01-27 17:49:35 +0100

    fix my mistake

--- components/UserBarBtn/UserBarBtn.tsx
+++ components/UserBarBtn/UserBarBtn.tsx
@@ -13,17 +13,13 @@ const UserBarBtn = ({ isOpen, onToggle }: Props) => {
   const { user, refreshUser, _hasHydrated } = useAuthStore();
 
   useEffect(() => {
-    // Якщо Zustand вже прочитав localStorage, а юзера все ще немає — питаємо сервер
     if (_hasHydrated && !user) {
       refreshUser();
     }
   }, [_hasHydrated, user, refreshUser]);
 
-  // Поки ми не впевнені, чи є юзер, краще показувати скелетон або пусту кнопку
-  // замість return null, щоб компонент залишався в дереві
   if (!_hasHydrated) return <div className={css.loaderPlaceholder} />;
 
-  // Якщо після refreshUser все одно null — тоді вже ховаємо
   if (!user) return null;
   const { name, avatarUrl } = user;
   const firstLetter = name.charAt(0).toUpperCase();
@@ -32,7 +28,7 @@ const UserBarBtn = ({ isOpen, onToggle }: Props) => {
     <button type="button" className={css.UserBarBtn} onClick={onToggle}>
       {/* Avatar */}
       {avatarUrl ? (
-        <Image src={avatarUrl} alt={name} className={css.avatar} />
+        <Image src={avatarUrl} alt={name} className={css.avatar} width={44} height={44}/>
       ) : (
         <div className={css.avatarFallback}>{firstLetter}</div>
       )}
