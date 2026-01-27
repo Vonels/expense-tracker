a49f0ec4861b7272a0875a5318c9870088217c0d
Vonels <klizka38@gmail.com>
2026-01-27 03:15:08 +0200

    vercel

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { useAuthStore } from "@/lib/store/authStore";
 
-const baseURL = (process.env.NEXT_PUBLIC_API_URL_FRONTEND || "") + "/api";
-
 export const api = axios.create({
-  baseURL,
+  baseURL: "/api",
   withCredentials: true,
 });
 
