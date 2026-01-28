16eee78642acdb33bdca2b0b62607f6183812d4d
Valeri <valeriakoval27072007@gmail.com>
2026-01-28 22:56:34 +0100

    photo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import css from "../page.module.css";
 import { Icon } from "@/components/Icon/Icon";
 
@@ -8,23 +9,29 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
+  const showBackground = pathname !== "/sign-in" && pathname !== "/sign-up";
+
   return (
     <main className={css.mainContainer}>
       <div className={css.wrapper}>
-        <div className={css.backgroundphoto}>
-          <div className={css.rectangle}>
-            <div className={css.svg}>
-              <Icon id="icon-arrow-up-right2" className={css.icon} />
-            </div>
-            <div>
-              <p className={css.rectangle_text1}>Your balance</p>
-              <p className={css.rectangle_text2}>$632.000</p>
-            </div>
-            <div className={css.rectangle_block2}>
-              <p className={css.rectangle_text3}>+1.29%</p>
+        {showBackground && (
+          <div className={css.backgroundphoto}>
+            <div className={css.rectangle}>
+              <div className={css.svg}>
+                <Icon id="icon-arrow-up-right2" className={css.icon} />
+              </div>
+              <div>
+                <p className={css.rectangle_text1}>Your balance</p>
+                <p className={css.rectangle_text2}>$632.000</p>
+              </div>
+              <div className={css.rectangle_block2}>
+                <p className={css.rectangle_text3}>+1.29%</p>
+              </div>
             </div>
           </div>
-        </div>
+        )}
 
         <div className={css.contentSide}>{children}</div>
       </div>
