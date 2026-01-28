165b8cc59236dff8be553eb55efeeae28d4b03b2
Valeri <valeriakoval27072007@gmail.com>
2026-01-28 23:06:26 +0100

    photo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -11,28 +11,27 @@ export default function AuthLayout({
 }) {
   const pathname = usePathname();
 
-  const showBackground = pathname !== "/sign-in" && pathname !== "/sign-up";
+  const isAuthPage = pathname === "/sign-in" || pathname === "/sign-up";
 
   return (
     <main className={css.mainContainer}>
       <div className={css.wrapper}>
-        {/* 4. Використовуємо умовний рендеринг */}
-        {showBackground && (
-          <div className={css.backgroundphoto}>
-            <div className={css.rectangle}>
-              <div className={css.svg}>
-                <Icon id="icon-arrow-up-right2" className={css.icon} />
-              </div>
-              <div>
-                <p className={css.rectangle_text1}>Your balance</p>
-                <p className={css.rectangle_text2}>$632.000</p>
-              </div>
-              <div className={css.rectangle_block2}>
-                <p className={css.rectangle_text3}>+1.29%</p>
-              </div>
+        <div
+          className={`${css.backgroundphoto} ${isAuthPage ? css.hiddenOnSmallScreens : ""}`}
+        >
+          <div className={css.rectangle}>
+            <div className={css.svg}>
+              <Icon id="icon-arrow-up-right2" className={css.icon} />
+            </div>
+            <div>
+              <p className={css.rectangle_text1}>Your balance</p>
+              <p className={css.rectangle_text2}>$632.000</p>
+            </div>
+            <div className={css.rectangle_block2}>
+              <p className={css.rectangle_text3}>+1.29%</p>
             </div>
           </div>
-        )}
+        </div>
 
         <div className={css.contentSide}>{children}</div>
       </div>
